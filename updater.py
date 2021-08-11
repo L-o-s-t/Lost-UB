e42b18a3370eb1c5c39fbe255be5619369b5b0a7
LOST <51839454+L-o-s-t@users.noreply.github.com>
2021-08-11 14:21:13 -0500

    update lost-ub updater
    this shit hurts my brain bruh

--- updater.py
+++ updater.py
@@ -4,7 +4,6 @@ import time
 import shutil
 import subprocess
 from git import Repo
-import stat
 
 print("""
                                   :::            ::::::::           ::::::::       :::::::::::
@@ -17,7 +16,6 @@ print("""
 
 ################################################# LOST.#0404 ###########################################################
 """)
-
 
 lostUB_new = requests.get("https://raw.githubusercontent.com/L-o-s-t/Lost-UB/main/bot.py")
 with open("bot_new.py", "w", encoding="utf-8") as new:
@@ -47,12 +45,25 @@ if os.path.exists("bot.py"):
         shutil.rmtree("dist")
         input("[LOST-UPDATER] You may now close this window")
     elif not os.path.exists("bot.exe"):
+        if not os.path.exists("data/bot.ico"):
+            print("[LOST-UPDATER] File not found, recovering bot.ico...")
+            Repo.clone_from("https://github.com/L-o-s-t/Lost-UB", "repo/").index.remove(['.github'],
+                                                                                        True, r=True)
+            os.remove("repo/bot.py")
+            os.remove("repo/commands.md")
+            os.remove("repo/README.md")
+            os.remove("repo/updater.py")
+            os.replace("repo/data/bot.ico", "data/bot.ico")
+            process = subprocess.run("echo y | rmdir /s repo",
+                                     shell=True,
+                                     stdout=subprocess.DEVNULL,
+                                     stderr=subprocess.STDOUT)
         os.remove("bot_new.py")
         print("[LOST-UPDATER] Executable not found, creating executable... Do not close until done.")
-        process = subprocess.Popen("pyinstaller --icon data\\bot.ico --onefile bot.py",
-                                   stdout=subprocess.DEVNULL,
-                                   stderr=subprocess.STDOUT)
-        process.wait()
+        process2 = subprocess.Popen("pyinstaller --icon data\\bot.ico --onefile bot.py",
+                                    stdout=subprocess.DEVNULL,
+                                    stderr=subprocess.STDOUT)
+        process2.wait()
         print("[LOST-UPDATER] Executable created, cleaning up...")
         shutil.rmtree("build")
         shutil.rmtree("__pycache__")
@@ -60,6 +71,21 @@ if os.path.exists("bot.py"):
         os.replace("dist/bot.exe", "bot.exe")
         shutil.rmtree("dist")
         input("[LOST-UPDATER] You may now close this window.")
+    elif not os.path.exists("data/bot.ico"):
+        print("[LOST-UPDATER] File not found, recovering bot.ico...")
+        Repo.clone_from("https://github.com/L-o-s-t/Lost-UB", "repo/").index.remove(['.github'],
+                                                                                    True, r=True)
+        os.remove("repo/bot.py")
+        os.remove("repo/commands.md")
+        os.remove("repo/README.md")
+        os.remove("repo/updater.py")
+        os.replace("repo/data/bot.ico", "data/bot.ico")
+        process = subprocess.run("echo y | rmdir /s repo",
+                                 shell=True,
+                                 stdout=subprocess.DEVNULL,
+                                 stderr=subprocess.STDOUT)
+        os.remove("bot_new.py")
+        input("[LOST-UPDATER] File recovered! You may now close this window.")
     else:
         os.remove("bot_new.py")
         input("[LOST-UPDATER] You are already up to date!")
@@ -69,15 +95,38 @@ else:
     os.remove("bot_new.py")
     Repo.clone_from("https://github.com/L-o-s-t/Lost-UB", "repo/").index.remove(['.github'],
                                                                                 True, r=True)
-    os.replace("repo/bot.exe", "bot.exe")
-    os.replace("repo/bot.py", "bot.py")
-    os.remove("repo/updater.exe")
-    shutil.copytree("repo/data", "data")
-    shutil.rmtree("repo/data")
-    os.replace("repo/commands.md", "commands.md")
-    os.replace("repo/README.md", "README.md")
+    if not os.path.exists("bot.py"):
+        os.replace("repo/bot.py", "bot.py")
+    else:
+        os.remove("repo/bot.py")
+    os.remove("repo/updater.py")
+    if not os.path.exists("data/bot.ico"):
+        if os.path.exists("data"):
+            shutil.rmtree("data")
+        shutil.copytree("repo/data", "data")
+    else:
+        shutil.rmtree("repo/data")
+    if not os.path.exists("commands.md"):
+        os.replace("repo/commands.md", "commands.md")
+    else:
+        os.remove("repo/commands.md")
+    if not os.path.exists("README.md"):
+        os.replace("repo/README.md", "README.md")
+    else:
+        os.remove("repo/README.md")
     process = subprocess.run("echo y | rmdir /s repo",
                              shell=True,
                              stdout=subprocess.DEVNULL,
                              stderr=subprocess.STDOUT)
+    if not os.path.exists("bot.exe"):
+        process2 = subprocess.Popen("pyinstaller --icon data\\bot.ico --onefile bot.py",
+                                    stdout=subprocess.DEVNULL,
+                                    stderr=subprocess.STDOUT)
+        process2.wait()
+        print("[LOST-UPDATER] Executable created, cleaning up...")
+        shutil.rmtree("build")
+        shutil.rmtree("__pycache__")
+        os.remove("bot.spec")
+        os.replace("dist/bot.exe", "bot.exe")
+        shutil.rmtree("dist")
     input("[LOST-UPDATER] Lost-UB was successfully installed. You may now close this window.")
