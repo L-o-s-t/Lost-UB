0399bf6678d921efe58dd44583a89bc482a496c8
LOST <51839454+L-o-s-t@users.noreply.github.com>
2021-08-31 22:57:26 -0500

    update serverinfo command

--- bot.py
+++ bot.py
@@ -1693,6 +1693,7 @@ async def serverinfo(ctx):
     if blacklist_check(ctx):
         await ctx.reply("You are blacklisted!")
     else:
+        log(ctx, "SERVERINFO")
         name = str(ctx.guild.name)
 
         owner = str(ctx.guild.owner)
@@ -1702,17 +1703,29 @@ async def serverinfo(ctx):
 
         icon = ctx.guild.icon_url
 
-        embed = discord.Embed(
-            title=name + "Server Info",
-            color=embedcolor()
-        )
-        embed.set_thumbnail(url=icon)
-        embed.add_field(name="owner", value=owner, inline=True)
-        embed.add_field(name="Server ID", value=guild_id, inline=True)
-        embed.add_field(name="Server Region", value=region, inline=True)
-        embed.add_field(name="Member Count", value=membercount, inline=True)
+        try:
+            embed = discord.Embed(
+                title=name + "Server Info",
+                color=embedcolor()
+            )
+            embed.set_thumbnail(url=icon)
+            embed.add_field(name="owner", value=owner, inline=True)
+            embed.add_field(name="Server ID", value=guild_id, inline=True)
+            embed.add_field(name="Server Region", value=region, inline=True)
+            embed.add_field(name="Member Count", value=membercount, inline=True)
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
+        except discord.Forbidden:
+            await simple_codeblock(ctx, f"[ Server Info ]\n"
+                                        f"{ctx.guild.name}\n\n"
+                                        f"[ Owner ]\n"
+                                        f"{owner}\n\n"
+                                        f"[ Server ID ]\n"
+                                        f"{guild_id}\n\n"
+                                        f"[ Server Region ]\n"
+                                        f"{region}\n\n"
+                                        f"[ Member Count ]\n"
+                                        f"{membercount}")
 
 
 # abc ==================================================================================================================
