cc2f1db8ce489562b63d8726e11e3a932d6c38d0
LOST <51839454+L-o-s-t@users.noreply.github.com>
2021-10-17 00:35:58 -0500

    added more customization for sendembed

--- commands/embed.py
+++ commands/embed.py
@@ -12,18 +12,55 @@ class Embed(commands.Cog):
             await ctx.message.delete()
             log(ctx, "ERROR", "Missing required arguments.")
         else:
-            title, description = arguments.split("| ")
-            await ctx.message.delete()
-            embed = discord.embeds.Embed(
-                title=title,
-                description=description,
-                colour=embedcolor()
-            )
-            footer(embed)
-            try:
-                await ctx.send(embed=embed)
-            except discord.Forbidden:
-                log(ctx, "ERROR", "Unable to send embedded message. Missing Permission(s).")
+            splits = 0
+            for x in arguments:
+                if x:
+                    if x == "|":
+                        splits += 1
+            if splits == 1:
+                title, description = arguments.split("| ")
+                await ctx.message.delete()
+                embed = discord.embeds.Embed(
+                    title=title,
+                    description=description,
+                    colour=embedcolor()
+                )
+                footer(embed)
+                try:
+                    await ctx.send(embed=embed)
+                except discord.Forbidden:
+                    log(ctx, "ERROR", "Unable to send embedded message. Missing Permission(s).")
+            elif splits == 2:
+                title, description, color = arguments.split("| ")
+                await ctx.message.delete()
+                embed = discord.embeds.Embed(
+                    title=title,
+                    description=description,
+                    colour=embedcolor(color)
+                )
+                footer(embed)
+                try:
+                    await ctx.send(embed=embed)
+                except discord.Forbidden:
+                    log(ctx, "ERROR", "Unable to send embedded message. Missing Permission(s).")
+            elif splits == 3:
+                title, description, color, thumbnail = arguments.split(" | ")
+                await ctx.message.delete()
+                embed = discord.embeds.Embed(
+                    title=title,
+                    description=description,
+                    colour=embedcolor(color)
+                )
+                embed.set_thumbnail(url=f"{thumbnail}")
+                footer(embed)
+                try:
+                    await ctx.send(embed=embed)
+                except discord.Forbidden:
+                    log(ctx, "ERROR", "Unable to send embedded message. Missing Permission(s).")
+            else:
+                await ctx.message.delete()
+                log(ctx, "SENDEMBED", f"Too many seperators! "
+                                      f"Usage: {get_prefix()}sendembed (title) | (description) | (color) | (thumbnail)")
 
 
 def setup(userbot):
