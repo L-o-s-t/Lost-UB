931b5565ef54123e13ca60aef490b87e0632ced9
LOST <51839454+L-o-s-t@users.noreply.github.com>
2022-01-13 11:54:02 -0600

    update pfp.py
    - updated permission check
    - updated logging
    - pfp command no longer requires an argument to run, if no argument is provided then the command author's profile picture is returned.

--- commands/pfp.py
+++ commands/pfp.py
@@ -9,7 +9,7 @@ class PFP(commands.Cog):
     @commands.command()
     async def stealpfp(self, ctx, member: discord.Member):
         if ctx.author == bot.user:
-            log(ctx, "STEALPFP")
+            await log(ctx, description="This user used the command STEALPFP", color=embedcolor())
             if config['CONFIGURATION']['silentsteal'] == "True":
                 await ctx.message.delete()
             elif config['CONFIGURATION']['silentsteal'] == "False":
@@ -51,44 +51,77 @@ class PFP(commands.Cog):
                     await bot.user.edit(avatar=image.read())
 
     @commands.command()
-    async def pfp(self, ctx, member: discord.Member):
-        if blacklist_check(ctx):
-            log(ctx, "BLACKLIST", f"{ctx.author.display_name} tried to use the command PFP.")
+    async def pfp(self, ctx, member: discord.Member = None):
+        if permission_check(ctx):
+            if config["CONFIGURATION"]["blacklist"] == "True":
+                await log(ctx, "BLACKLIST", "This user attempted to use PFP", color=embedcolor("red"))
+            elif config["CONFIGURATION"]["whitelist"] == "True":
+                await log(ctx, "WHITELIST", "This user attempted to use PFP", color=embedcolor("red"))
         else:
-            log(ctx, "PFP")
+            await log(ctx, description="This user used the command PFP", color=embedcolor())
             try:
-                embed = discord.embeds.Embed(
-                    title="Profile Picture",
-                    colour=embedcolor()
-                )
-                embed.add_field(
-                    name="Link",
-                    value=f"[Click Me]({member.avatar_url_as(format='jpg')})"
-                )
-                embed.add_field(
-                    name="User",
-                    value=f"{member.display_name}"
-                )
-                embed.set_thumbnail(url=member.avatar_url)
-                embed.set_footer(
-                    text=f"Logged in as {bot.user} | Lost-UB",
-                    icon_url=bot.user.avatar_url
-                )
-                await ctx.reply(embed=embed)
+                if member is None:
+                    embed = discord.embeds.Embed(
+                        title="Profile Picture",
+                        colour=embedcolor()
+                    )
+                    embed.add_field(
+                        name="Link",
+                        value=f"[Click Me]({ctx.author.avatar_url_as(format='jpg')})"
+                    )
+                    embed.add_field(
+                        name="User",
+                        value=f"{ctx.author.display_name}"
+                    )
+                    embed.set_thumbnail(url=ctx.author.avatar_url)
+                    embed.set_footer(
+                        text=f"Logged in as {bot.user} | Lost-UB",
+                        icon_url=bot.user.avatar_url
+                    )
+                    await ctx.reply(embed=embed)
+                else:
+                    embed = discord.embeds.Embed(
+                        title="Profile Picture",
+                        colour=embedcolor()
+                    )
+                    embed.add_field(
+                        name="Link",
+                        value=f"[Click Me]({member.avatar_url_as(format='jpg')})"
+                    )
+                    embed.add_field(
+                        name="User",
+                        value=f"{member.display_name}"
+                    )
+                    embed.set_thumbnail(url=member.avatar_url)
+                    embed.set_footer(
+                        text=f"Logged in as {bot.user} | Lost-UB",
+                        icon_url=bot.user.avatar_url
+                    )
+                    await ctx.reply(embed=embed)
             except discord.Forbidden:
-                await ctx.reply(f"```ini\n"
-                                f"[ Profile Picture ]\n\n"
-                                f"[ Link ]\n"
-                                f"{member.avatar_url}\n\n"
-                                f"[ User ]\n"
-                                f"{member.display_name}\n\n"
-                                f"{codeblock_footer()}\n"
-                                f"```")
+                if member is None:
+                    await ctx.reply(f"```ini\n"
+                                    f"[ Profile Picture ]\n\n"
+                                    f"[ Link ]\n"
+                                    f"{ctx.author.avatar_url}\n\n"
+                                    f"[ User ]\n"
+                                    f"{ctx.author.display_name}\n\n"
+                                    f"{codeblock_footer()}\n"
+                                    f"```")
+                else:
+                    await ctx.reply(f"```ini\n"
+                                    f"[ Profile Picture ]\n\n"
+                                    f"[ Link ]\n"
+                                    f"{member.avatar_url}\n\n"
+                                    f"[ User ]\n"
+                                    f"{member.display_name}\n\n"
+                                    f"{codeblock_footer()}\n"
+                                    f"```")
 
     @commands.command()
     async def savepfp(self, ctx, member: discord.Member):
         if ctx.author == bot.user:
-            log(ctx, "PFP")
+            await log(ctx, description="This user used the command SAVEPFP", color=embedcolor())
             if config['CONFIGURATION']['silentsave'] == "True":
                 await ctx.message.delete()
             elif config['CONFIGURATION']['silentsave'] == "False":
