473c97e723eaeb5a45d7abe1f2420574df6ff481
Lost <51839454+L-o-s-t@users.noreply.github.com>
2024-05-20 13:27:19 -0700

    improved afk
    - afk feature is now triggered when a user messages bot user in direct messages. this used to only be triggered when bot user is mentioned.
    - code blocks are now sent instead of regular messages.

--- commands/afk.py
+++ commands/afk.py
@@ -13,11 +13,13 @@ class AFK(commands.Cog):
             if config["CONFIGURATION"]["AFK"] == "True":
                 config["CONFIGURATION"]["AFK"] = "False"
                 write()
-                await ctx.reply("Successfully set! AFK has been turned off.")
+                await simple_codeblock(ctx, f"[ AFK ]\n"
+                                            f"Successfully set! AFK has been turned off.")
             elif config["CONFIGURATION"]["AFK"] == "False":
                 config["CONFIGURATION"]["AFK"] = "True"
                 write()
-                await ctx.reply("Successfully set! AFK has been turned on.")
+                await simple_codeblock(ctx, f"[ AFK ]\n"
+                                            f"Successfully set! AFK has been turned on.")
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
@@ -28,6 +30,17 @@ class AFK(commands.Cog):
                     async with ctx.channel.typing():
                         await asyncio.sleep(5)
                 await ctx.reply(config["CONFIGURATION"]["afk_msg"])
+            elif ctx.guild is None and ctx.author != bot.user:
+                if config['CONFIGURATION']['afk_legit'] == "True":
+                    time.sleep(random.randrange(5, 10))
+                    async with ctx.channel.typing():
+                        await asyncio.sleep(5)
+                await ctx.reply(config["CONFIGURATION"]["afk_msg"])
+            config['CONFIGURATION']['AFK'] = "False"
+            write()
+            await asyncio.sleep(30)
+            config['CONFIGURATION']['AFK'] = "True"
+            write()
 
 
 def setup(userbot):
