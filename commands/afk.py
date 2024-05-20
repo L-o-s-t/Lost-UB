f111085ed7190b729c29a1a0c6c2e06dbea5f852
Lost <51839454+L-o-s-t@users.noreply.github.com>
2024-05-20 14:31:27 -0700

    fixed afk

--- commands/afk.py
+++ commands/afk.py
@@ -10,37 +10,42 @@ class AFK(commands.Cog):
     async def afk(self, ctx):
         if ctx.author == bot.user:
             print(log(ctx, description="This user used the command AFK"))
-            if config["CONFIGURATION"]["AFK"] == "True":
-                config["CONFIGURATION"]["AFK"] = "False"
+            if config["CONFIGURATION"]["afk"] == "True":
+                config["CONFIGURATION"]["afk"] = "False"
                 write()
                 await simple_codeblock(ctx, f"[ AFK ]\n"
                                             f"Successfully set! AFK has been turned off.")
-            elif config["CONFIGURATION"]["AFK"] == "False":
-                config["CONFIGURATION"]["AFK"] = "True"
+            elif config["CONFIGURATION"]["afk"] == "False":
+                config["CONFIGURATION"]["afk"] = "True"
                 write()
                 await simple_codeblock(ctx, f"[ AFK ]\n"
                                             f"Successfully set! AFK has been turned on.")
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if config["CONFIGURATION"]["AFK"] == "True":
+        if config["CONFIGURATION"]["afk"] == "True":
             if f'<@{bot.user.id}>' in ctx.content or f'<@!{bot.user.id}>' in ctx.content:
                 if config['CONFIGURATION']['afk_legit'] == "True":
                     time.sleep(random.randrange(5, 10))
                     async with ctx.channel.typing():
                         await asyncio.sleep(5)
                 await ctx.reply(config["CONFIGURATION"]["afk_msg"])
+                config["CONFIGURATION"]["afk"] = "False"
+                write()
+                await asyncio.sleep(30)
+                config["CONFIGURATION"]["afk"] = "True"
+                write()
             elif ctx.guild is None and ctx.author != bot.user:
                 if config['CONFIGURATION']['afk_legit'] == "True":
                     time.sleep(random.randrange(5, 10))
                     async with ctx.channel.typing():
                         await asyncio.sleep(5)
                 await ctx.reply(config["CONFIGURATION"]["afk_msg"])
-            config['CONFIGURATION']['AFK'] = "False"
-            write()
-            await asyncio.sleep(30)
-            config['CONFIGURATION']['AFK'] = "True"
-            write()
+                config["CONFIGURATION"]["afk"] = "False"
+                write()
+                await asyncio.sleep(30)
+                config["CONFIGURATION"]["afk"] = "True"
+                write()
 
 
 def setup(userbot):
